ca3b3b3db657f2df8f305e99bea3b7904853ccc8
Neville Dipale <nevilledips@gmail.com>
2017-03-18 10:54:51 +0200

    Measurement: Distance - get distance of 2 Points, includes unit tests

--- src/main/kotlin/za/co/movinggauteng/turfkotlin/measurement/Distance.kt
+++ src/main/kotlin/za/co/movinggauteng/turfkotlin/measurement/Distance.kt
@@ -4,8 +4,7 @@ import za.co.movinggauteng.turfkotlin.geojson.Point
 import za.co.movinggauteng.turfkotlin.geojson.getCoord
 import za.co.movinggauteng.turfkotlin.helpers.Factors
 import za.co.movinggauteng.turfkotlin.helpers.Units
-import za.co.movinggauteng.turfkotlin.helpers.point
-import java.lang.Math.*
+import java.lang.StrictMath.*
 
 /**
  * Created by Neville on 25 Feb 2017.
@@ -16,12 +15,12 @@ fun distance(start: Point, end: Point, units: Units = Units.KILOMETERS) : Double
     val coordinates1 = start.getCoord()
     val coordinates2 = end.getCoord()
 
-    // adapted from https://github.com/acmeism/RosettaCodeData/blob/master/Task/Haversine-formula/Kotlin/haversine-formula.kotlin
-    val λ1 = toRadians(coordinates1.second)
-    val λ2 = toRadians(coordinates2.second)
-    val Δλ = toRadians(λ2 - λ1)
-    val Δφ = toRadians(coordinates2.first - coordinates1.first)
-    return 2 * Factors[units]!! * asin(sqrt(pow(sin(Δλ / 2), 2.0) + pow(sin(Δφ / 2), 2.0) * cos(λ1) * cos(λ2)))
-}
+    val dLat = toRadians(coordinates2.second - coordinates1.second)
+    val dLon = toRadians(coordinates2.first - coordinates1.first)
+    val lat1 = toRadians(coordinates1.second)
+    val lat2 = toRadians(coordinates2.second)
 
-fun main(args: Array<String>) = println("result: " + distance(point(listOf(-86.67, 36.12)), point(listOf(-118.40, 33.94))))
+    val a = pow(sin(dLat/2), 2.0) + pow(sin(dLon/2), 2.0) * cos(lat1) * cos(lat2)
+
+    return 2 * Factors[units]!! * atan2(sqrt(a), sqrt(1-a))
+}
---
+++ src/test/kotlin/za/co/movinggauteng/turfkotlin/measurement/DistanceTest.kt
@@ -0,0 +1,29 @@
+package za.co.movinggauteng.turfkotlin.measurement
+
+import io.kotlintest.specs.FunSpec
+import za.co.movinggauteng.turfkotlin.geojson.Point
+import za.co.movinggauteng.turfkotlin.helpers.Units
+
+/**
+ * Created by Neville on 18 Mar 2017.
+ */
+
+class DistanceTest : FunSpec() {
+    init {
+        val pt1 = Point()
+        pt1.coordinates = listOf(-75.343, 39.984)
+        val pt2 = Point()
+        pt2.coordinates = listOf(-75.534, 39.123)
+
+        val kmTolerance = (97.159578031319 plusOrMinus 0.0000001)
+
+        test("Distance calculation should return the correct distance for all units") {
+            distance(pt1, pt2, Units.MILES) shouldBe (60.372184058374906 plusOrMinus 0.000001)
+            distance(pt1, pt2, Units.KILOMETERS) shouldBe kmTolerance
+//            distance(pt1, pt2, Units.KILOMETERS) shouldBe kmTolerance
+            distance(pt1, pt2, Units.RADIANS) shouldBe (0.015245501024842147 plusOrMinus 0.000001)
+            distance(pt1, pt2, Units.DEGREES) shouldBe (0.8735028650863798 plusOrMinus 0.000001)
+            distance(pt1, pt2) shouldBe kmTolerance
+        }
+    }
+}
